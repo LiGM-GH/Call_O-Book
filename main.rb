36ba64f603f84ef4f7b8a7167725ca1533d2cbe2
LiGM-GH <82590325+LiGM-GH@users.noreply.github.com>
2021-11-19 11:10:15 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,25 +2,15 @@ require_relative "./program_files/file_reader.rb"
 require_relative "./program_files/errors.rb"
 require_relative "./program_files/game_processor.rb"
 
-#~  Что нужно сделать? 
-#~  Берём файл, читаем, выводим вопросы, вводим ответы. 
-#~  Как читаем - без разницы, как выводим - без разницы. 
-#~  Как вводим ответы - без разницы. 
-
-#~  Главное:    читаем, спрашиваем, слушаем, повторяем. 
-#~  На_будущее: можно добавить выбор имени, версии, файла сюжета. 
-#~  На_будущее: нужно добавить GUI. 
-
+# MAIN:   Say, ask, get, repeat. 
+# FUTURE: add name, version, plot file. 
+# FUTURE: Needed GUI. 
+plot      = "#{__dir__}/program_files/data/plot.txt"
+questions = "#{__dir__}/program_files/data/questions.txt"
 GameProcessor.new do |game|
-    game.start(
-        "./program_files/data/plot.txt", 
-        "./program_files/data/questions.txt"
-    )
-    game.cycle do 
-        game.say;
-        game.ask;
-        game.get;
-        game.update;
-    end
-    game.end
+  game.start(plot, questions)
+  while game.say
+    game.ask
+    game.get
+  end
 end
