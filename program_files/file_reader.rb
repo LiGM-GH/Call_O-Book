caa4c0b3a1675a1c8bfa55636926a22f6118df9f
LiGM-GH <82590325+LiGM-GH@users.noreply.github.com>
2021-11-19 11:12:02 +0300

    Update file_reader.rb

--- program_files/file_reader.rb
+++ program_files/file_reader.rb
@@ -1,8 +1,36 @@
 require_relative "contents_table_manager.rb"
+require_relative "errors.rb"
 class FileReader < ContentsTableManager
-    def initialize(file)
-        super(file)
-        @file_string = file
+  attr_accessor :file_string, :table_string
+  def initialize(file)
+    super(file)
+  end
+  def find(branch)
+    i = 0
+    File.open(table_string, 'r') do |file|
+      line = ""
+      until file.eof? || line == 'Act ' + branch + "\n" || line == 'Ask ' + branch + "\n"
+        line = file.readline
+      end 
+      if line == 'Act ' + branch + "\n" || line == 'Ask ' + branch + "\n"
+        i = file.readline.to_i
+      else
+        # puts line
+        return false
+      end
     end
+    # puts "line: #{i}"
+    i
+  end
+  def get_scene(branch)
+    str = []
+    if (line = find branch)
+      File.open file_string, 'r' do |file|
+        line.times { file.readline } # Skip to line
+        str << line.chomp until (line = file.readline) == "\n"
+      end
+    else raise NoLineError.new(""), "No such line!"
+    end
+    str
+  end
 end
-#~  FileReader.new("#{File.dirname(__FILE__)}/data/plot.txt")
